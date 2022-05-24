8573438b10e0702c17e2f697183a72c2cfcb7bec
mikoogla9 <rgmikoogla@gmail.com>
2022-05-24 15:08:22 +0200

    button changing value

--- workout-app/src/App.js
+++ workout-app/src/App.js
@@ -4,9 +4,18 @@ import Dashboard from './components/Dashboard/Dashboard';
 import LandingPage from './components/LandingPage/LandingPage';
 import UserLogin from './components/UserLogin/UserLogin';
 
+let value = 1;
+
+function changeValue(){
+  value = 2*value;
+  console.log(value);
+  document.getElementById("a").textContent = value;
+  }
+
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
+    <button id="a" onClick={changeValue}>Guzik</button>
     <LandingPage />
     <UserLogin/>
     <Dashboard/>
