62002bb7063b4ccbd0a6d277de6366a6426c00d1
mikoogla9 <rgmikoogla@gmail.com>
2022-06-10 02:12:38 +0200

    Added useState to button, changing exercise name

--- workout-app/src/components/Course/Excercises/ExcercisesList.js
+++ workout-app/src/components/Course/Excercises/ExcercisesList.js
@@ -3,12 +3,13 @@ import Exercise from './Exercise';
 import './course.css'
 
 function ExcercisesList() {
+    const buttonContent = "Change";
     const excercises = [
-        { exName: 'Pullups', exDate: new Date(2022, 6, 6), exData: '5 | 5 | 4 | 2', exInfo: 'More info' },
-        { exName: 'Bench press', exDate: new Date(2022, 4, 23), exData: '40x10 | 40x8 | 40x6 | 40x5', exInfo: 'More info' },
-        { exName: 'Barebells', exDate: new Date(2022, 5, 11), exData: '10x10 | 10x10 | 10x10 | 10x10', exInfo: 'More info' },
-        { exName: 'Plank', exDate: new Date(2022, 5, 12), exData: '1min | 1min | 1min', exInfo: 'More info' },
-        { exName: 'Pushups', exDate: new Date(2022, 6, 6), exData: '15 | 12 | 9 | 8', exInfo: 'More info' }
+        { exName: 'Pullups', exDate: new Date(2022, 6, 6), exData: '5 | 5 | 4 | 2', exInfo: buttonContent },
+        { exName: 'Bench press', exDate: new Date(2022, 4, 23), exData: '40x10 | 40x8 | 40x6 | 40x5', exInfo: buttonContent },
+        { exName: 'Barebells', exDate: new Date(2022, 5, 11), exData: '10x10 | 10x10 | 10x10 | 10x10', exInfo: buttonContent },
+        { exName: 'Plank', exDate: new Date(2022, 5, 12), exData: '1min | 1min | 1min', exInfo: buttonContent },
+        { exName: 'Pushups', exDate: new Date(2022, 6, 6), exData: '15 | 12 | 9 | 8', exInfo: buttonContent }
     ]
     let exList = [];
     for (let index = 0; index < excercises.length; index++) {
--- workout-app/src/components/Course/Excercises/Exercise.js
+++ workout-app/src/components/Course/Excercises/Exercise.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from "@mui/material/Button";
 import CalendarItem from './CalendarItem';
 import Card from '../UI/Card';
 
+
+
 function Exercise(props) {
 
+    const [name, setName] = useState(props.data.exName);
+
+    const ClickHandler = () => {
+        console.log("Clicked*");
+        setName("changed");
+    }
     return (
         <Card className='excercise-item'>
             <div className='excercise-left'>
                 <CalendarItem data={props.data} />
-                <div className='excercise-item__name'>{props.data.exName}</div>
+                <div className='excercise-item__name'>{name}</div>
             </div>
             <div className='excercise-right'>
                 <div className='excercise-item__data'>{props.data.exData}</div>
-                <Button variant="outlined">{props.data.exInfo}</Button>
+                <Button onClick={ClickHandler} variant="outlined">{props.data.exInfo}</Button>
             </div>
         </Card>
     );
