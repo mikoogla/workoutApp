f4b30aa6426b4ea82efbaf9fbb1511ccfc3172be
mikoogla9 <rgmikoogla@gmail.com>
2022-06-10 18:33:13 +0200

    button fixed

--- workout-app/src/components/Course/Excercises/Exercise.js
+++ workout-app/src/components/Course/Excercises/Exercise.js
@@ -1,32 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
-import CalendarItem from './CalendarItem';
-import Card from '../UI/Card';
-
-
+import CalendarItem from "./CalendarItem";
+import Card from "../UI/Card";
 
 function Exercise(props) {
+  const [name, setName] = useState(props.data.exName);
 
-    const [name, setName] = useState(props.data.exName);
-
-    const ClickHandler = () => {
-        if (name === "hidden")
-            setName(props.data.exName);
-        else
-            setName("hidden");
-    }
-    return (
-        <Card className='excercise-item'>
-            <div className='excercise-left'>
-                <CalendarItem data={props.data} />
-                <div className='excercise-item__name'>{name}</div>
-            </div>
-            <div className='excercise-right'>
-                <div className='excercise-item__data'>{props.data.exData}</div>
-                <Button onClick={ClickHandler} variant="outlined" className="change--button">{props.data.exInfo}</Button>
-            </div>
-        </Card>
-    );
+  const ClickHandler = () => {
+    if (name === "hidden") setName(props.data.exName);
+    else setName("hidden");
+  };
+  return (
+    <Card className="excercise-item">
+      <div className="excercise-left">
+        <CalendarItem data={props.data} />
+        <div className="excercise-item__name">{name}</div>
+      </div>
+      <div className="excercise-right">
+        <div className="excercise-item__data">{props.data.exData}</div>
+        <Button onClick={ClickHandler} variant="outlined">
+          {props.data.exInfo}
+        </Button>
+      </div>
+    </Card>
+  );
 }
 
-export default Exercise
+export default Exercise;
