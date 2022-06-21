7ae21df77c1a2a6ab4937878b196073ef957a21d
mikoogla9 <rgmikoogla@gmail.com>
2022-06-21 15:52:02 +0200

    login and signup forms simplified

--- workout-app/src/components/UserLogin/UserLogin.js
+++ workout-app/src/components/UserLogin/UserLogin.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Button from "../UI/Button/Button";
-
+import Input from "../UI/Input/Input";
+import styles from "./UserLogin.module.css";
+import Card from "../UI/Card/Card";
 function UserLogin(props) {
   const loginHandler = () => {
     props.onLogin();
@@ -11,10 +13,24 @@ function UserLogin(props) {
   };
 
   return (
-    <>
-      <Button onClick={loginHandler}>FAST LOGIN</Button>
-      <Button onClick={logoutHandler}>LOGOUT</Button>
-    </>
+    <div className={styles.container}>
+      <Card className={styles.login}>
+        <div className={styles.login_title}>Login</div>
+        <div className={styles.login_input}>
+          <Input label="e-mail" type="email" />
+          <Input label="password" type="password" />
+        </div>
+        <div className={styles.login_buttons}>
+          <Button onClick={loginHandler}>LOGIN</Button>
+        </div>
+        <div className={styles.login_footer}>
+          No account?{" "}
+          <a className={styles.footer_registerLink} href="/sign-up">
+            Register
+          </a>
+        </div>
+      </Card>
+    </div>
   );
 }
 
