095729d2fde2116a5e92de48b55b088a9d78e9b3
mikoogla9 <rgmikoogla@gmail.com>
2022-07-04 16:27:14 +0200

    classes implementation

--- workout-app/src/components/class_components/Users.js
+++ workout-app/src/components/class_components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Component } from "react";
 import Button from "../UI/Button/Button";
 import User from "./User";
 
@@ -10,29 +10,40 @@ const DUMMY_USERS = [
   { id: "u3", name: "Julie" },
 ];
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showUsers: true,
+    };
+  }
+  toggleUsersHandler() {
+    //merges not overwrites
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers };
+    });
+  }
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
 
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
-  };
-
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
-
-  return (
-    <div className={classes.users}>
-      <Button className={classes.button} onClick={toggleUsersHandler}>
-        {showUsers ? "Hide" : "Show"} Users
-      </Button>
-      {showUsers && usersList}
-    </div>
-  );
-};
+    return (
+      <div className={classes.users}>
+        <Button
+          className={classes.button}
+          onClick={this.toggleUsersHandler.bind(this)}
+        >
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </Button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
 
 export default Users;
