89aad764b5a964ed4fe338b7a5ea210a2437fb08
mikoogla9 <rgmikoogla@gmail.com>
2022-07-08 00:34:59 +0200

    form

--- workout-app/src/components/Forms/Form.js
+++ workout-app/src/components/Forms/Form.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Button from "../UI/Button/Button";
 import Card from "../UI/Card/Card";
 import Input from "../UI/Input/Input";
 import styles from "./Form.module.css";
 export default function Form() {
+  const [enteredText, setEnteredText] = useState("");
+  const [Valid, setValid] = useState(false);
+  const [Touched, setTouched] = useState(false);
+  const Error = () => !Valid && Touched;
+  const content = useRef("");
+
+  const handleInput = (event) => {
+    setEnteredText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    setTouched(true);
+    event.preventDefault();
+
+    if (content.current.value.trim() === "") {
+      setValid(false);
+      return;
+    }
+    setValid(true);
+    setEnteredText(content.current.value);
+    console.log(content.current.value);
+  };
+  const Warning = () =>
+    Error() && <div className={styles.red}>Enter some content</div>;
+
   return (
     <Card className={styles.main}>
       <form className={styles.form}>
-        <Input type="text" placeholder="type content" />
-        <Button>Submit</Button>
+        Content:
+        <Input
+          ref={content}
+          placeholder="type content"
+          className={`${!Error() ? " " : styles.inputError}`}
+        />
+        <Warning />
+        <Button onClick={handleSubmit}>Submit</Button>
       </form>
     </Card>
   );
--- workout-app/src/components/UI/Input/Input.js
+++ workout-app/src/components/UI/Input/Input.js
@@ -1,7 +1,13 @@
 import React, { forwardRef } from "react";
 import styles from "./Input.module.css";
 const Input = forwardRef((props, ref) => {
-  return <input {...props} ref={ref} className={styles.input} />;
+  return (
+    <input
+      {...props}
+      ref={ref}
+      className={`${styles.input} ${props.className}`}
+    />
+  );
 });
 
 export default Input;
