b2815c928b9ae997965db069aa842b676062d395
mikoogla9 <rgmikoogla@gmail.com>
2022-07-08 18:46:58 +0200

    email input validation added to Form.js

--- workout-app/src/components/Forms/Form.js
+++ workout-app/src/components/Forms/Form.js
@@ -3,36 +3,60 @@ import Button from "../UI/Button/Button";
 import Card from "../UI/Card/Card";
 import Input from "../UI/Input/Input";
 import styles from "./Form.module.css";
+
+function emailIsValid(email) {
+  return /\S+@\S+\.\S+/.test(email);
+}
+
 export default function Form() {
-  const [enteredText, setEnteredText] = useState("");
-  const [Touched, setTouched] = useState(false);
-  const Valid = enteredText.trim() !== "";
+  const [enteredText, setEnteredText] = useState({ text: "", email: "" });
+  const [Touched, setTouched] = useState({ text: false, email: false });
+  const Valid = {
+    text: enteredText.text.trim() !== "",
+    email: emailIsValid(enteredText.email),
+  };
   const FormValid = () => {
-    if (Valid) return true;
+    if (Valid.text && Valid.email) return true;
     else return false;
   };
 
   const handleChange = (event) => {
-    setEnteredText(event.target.value);
+    if (event.target.type === "text") {
+      setTouched({ ...Touched, text: true });
+      setEnteredText({ ...enteredText, text: event.target.value });
+    }
+    if (event.target.type === "email") {
+      setTouched({ ...Touched, email: true });
+      setEnteredText({ ...enteredText, email: event.target.value });
+    }
+    // console.log({ ...enteredText, text: event.target.value });
+    // console.log(Touched);
   };
 
   const handleBlur = (event) => {
-    setTouched(true);
-    if (!Valid) return;
+    setTouched({ ...Touched, [event.target.type]: true });
+
+    if (!FormValid()) return;
   };
 
   const handleSubmit = (event) => {
-    setTouched(true);
+    setTouched({ ...Touched, [event.target.type]: true });
     event.preventDefault();
 
-    if (!Valid) return;
-    setTouched(false);
+    if (!FormValid()) return;
+
+    setTouched({ text: false, email: false });
     console.log(enteredText);
-    setEnteredText(" ");
+    setEnteredText({ text: "", email: "" });
   };
 
-  const Warning = () =>
-    !Valid && Touched && <div className={styles.red}>Enter some content</div>;
+  const TextWarning = () =>
+    !Valid.text &&
+    Touched.text && <div className={styles.red}>Enter some content</div>;
+
+  const EmailWarning = () =>
+    !Valid.email &&
+    Touched.email && <div className={styles.red}>Enter a valid email</div>;
 
   return (
     <Card className={styles.main}>
@@ -42,10 +66,19 @@ export default function Form() {
           placeholder="type content"
           onBlur={handleBlur}
           onChange={handleChange}
-          className={`${!Valid && Touched && styles.inputError}`}
-          value={enteredText}
+          className={`${!Valid.text && Touched.text && styles.inputError}`}
+          defaultValue={enteredText.text}
         />
-        <Warning />
+        <TextWarning />
+        Mail:
+        <Input
+          type="email"
+          onBlur={handleBlur}
+          onChange={handleChange}
+          className={`${!Valid.email && Touched.email && styles.inputError}`}
+          defaultValue={enteredText.email}
+        />
+        <EmailWarning />
         <Button
           className={!FormValid() && styles.disabled}
           disabled={!FormValid()}
