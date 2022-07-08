c034cb25119b825600d234a5b8e5619564e95cf2
mikoogla9 <rgmikoogla@gmail.com>
2022-07-08 16:21:26 +0200

    refactored

--- workout-app/src/components/Forms/Form.js
+++ workout-app/src/components/Forms/Form.js
@@ -1,53 +1,45 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import Button from "../UI/Button/Button";
 import Card from "../UI/Card/Card";
 import Input from "../UI/Input/Input";
 import styles from "./Form.module.css";
 export default function Form() {
   const [enteredText, setEnteredText] = useState("");
-  const [Valid, setValid] = useState(false);
   const [Touched, setTouched] = useState(false);
-  const Error = () => !Valid && Touched;
-  const content = useRef("");
+  const Valid = enteredText.trim() !== "";
 
   const handleChange = (event) => {
-    setValid(true);
     setEnteredText(event.target.value);
   };
+
   const handleBlur = (event) => {
     setTouched(true);
-    if (enteredText.trim() === "") {
-      setValid(false);
-      return;
-    }
-    setEnteredText(event.target.value);
+    if (!Valid) return;
   };
 
   const handleSubmit = (event) => {
     setTouched(true);
     event.preventDefault();
 
-    if (content.current.value.trim() === "") {
-      setValid(false);
-      return;
-    }
-    setValid(true);
-    setEnteredText(content.current.value);
-    console.log(content.current.value);
+    if (!Valid) return;
+    setTouched(false);
+    console.log(enteredText);
+    setEnteredText(" ");
   };
+
   const Warning = () =>
-    Error() && <div className={styles.red}>Enter some content</div>;
+    !Valid && Touched && <div className={styles.red}>Enter some content</div>;
 
   return (
     <Card className={styles.main}>
       <form className={styles.form}>
         Content:
         <Input
-          ref={content}
           placeholder="type content"
           onBlur={handleBlur}
           onChange={handleChange}
-          className={`${!Error() ? " " : styles.inputError}`}
+          className={`${!Valid && Touched && styles.inputError}`}
+          value={enteredText}
         />
         <Warning />
         <Button onClick={handleSubmit}>Submit</Button>
