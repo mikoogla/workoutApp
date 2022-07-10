3e872c6d87c4eaef305921acca4fd878281d6cb9
mikoogla9 <rgmikoogla@gmail.com>
2022-07-10 18:15:19 +0200

    quick update

--- workout-app/src/components/CustomHooks/Tasks/TaskList.js
+++ workout-app/src/components/CustomHooks/Tasks/TaskList.js
@@ -2,7 +2,6 @@ import React from "react";
 import TaskItem from "./TaskItem";
 
 export default function TaskList(props) {
-  console.log("task " + props.tasks[0].text);
   return (
     <>
       {props.tasks.map((task) => (
