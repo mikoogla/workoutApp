4a328035040b59fe34f2802a0ca12af7044acbb4
xtcek <mateusz.greda@tutore.eu>
2022-07-13 14:35:07 +0200

    add index iterating, make dynamic styling while button is clicked

--- workout-app/src/components/LandingPage/Carousel/Carousel.js
+++ workout-app/src/components/LandingPage/Carousel/Carousel.js
@@ -1,14 +1,42 @@
-import React from "react"
+import React, { useState } from "react"
 
 import "./Carousel.css"
 
 const Carousel = ({ children }) => {
+	const [activeIndex, setActiveIndex] = useState(0)
+
+	const updateIndex = newIndex => {
+		if (newIndex < 0) {
+			newIndex = 0
+		} else if (newIndex >= React.Children.count(children)) {
+			newIndex = React.Children.count(children) - 1
+		}
+
+		setActiveIndex(newIndex)
+	}
+
 	return (
 		<div className='carousel'>
-			<div className='carousel-inner' style={{ transform: "translateX(-0%)" }}>
+			<div
+				className='carousel-inner'
+				style={{ transform: `translateX(-${activeIndex * 100}%)` }}>
 				{React.Children.map(children, (child, index) => {
 					return React.cloneElement(child, { width: "100%" })
 				})}
+			</div>
+			<div className='indicators'>
+				<button onClick={() => updateIndex(activeIndex - 1)}> Prev </button>
+				{React.Children.map(children, (child, index) => {
+					return (
+						<button
+							className={`${index === activeIndex ? "active" : ""}`}
+							onClick={() => updateIndex(index)}>
+							{index + 1}
+						</button>
+					)
+				})}
+
+				<button onClick={() => updateIndex(activeIndex + 1)}> Next </button>
 			</div>
 		</div>
 	)
