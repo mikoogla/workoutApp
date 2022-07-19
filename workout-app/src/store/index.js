d25ccf4606322d83d45285844b68aaefd5abd693
mikoogla9 <rgmikoogla@gmail.com>
2022-07-19 22:51:06 +0200

    quick update, fixed unexpected behavior

--- workout-app/src/store/index.js
+++ workout-app/src/store/index.js
@@ -5,17 +5,19 @@ const initial_state = { counter: 1001, isVisible: true };
 const counterReducer = (state = initial_state, action) => {
   if (action.type === "increment") {
     return {
+      ...state,
       counter: state.counter + action.value,
     };
   }
   if (action.type === "decrement") {
     return {
+      ...state,
       counter: state.counter - action.value,
     };
   }
   if (action.type === "hideAndShow") {
     return {
-      counter: state.counter,
+      ...state,
       isVisible: !state.isVisible,
     };
   }
