69eff1c8def10dc76a6521198dcd165580b4ae6d
czesster <bloch.jakub@gmail.com>
2022-07-20 12:08:10 +0200

    fixed landing page carousel buttons

--- workout-app/src/components/LandingPage/Carousel/Carousel.js
+++ workout-app/src/components/LandingPage/Carousel/Carousel.js
@@ -1,52 +1,63 @@
-import React, { Fragment, useState } from "react"
-import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai"
+import React, { Fragment, useState } from "react";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
-import "./Carousel.css"
+import "./Carousel.css";
 
 const Carousel = ({ children }) => {
-	const [activeIndex, setActiveIndex] = useState(0)
+  const [activeIndex, setActiveIndex] = useState(0);
 
-	const updateIndex = newIndex => {
-		if (newIndex < 0) {
-			newIndex = React.Children.count(children) - 1
-		} else if (newIndex >= React.Children.count(children)) {
-			newIndex = 0
-		}
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = React.Children.count(children) - 1;
+    } else if (newIndex >= React.Children.count(children)) {
+      newIndex = 0;
+    }
 
-		setActiveIndex(newIndex)
-	}
+    setActiveIndex(newIndex);
+  };
 
-	return (
-		<Fragment>
-			<div className='carousel'>
-				<div className='arrows'>
-					<span onClick={() => updateIndex(activeIndex - 1)}>
-						<AiOutlineArrowLeft size='20px' />
-					</span>
-					<span onClick={() => updateIndex(activeIndex + 1)}>
-						<AiOutlineArrowRight size='20px' />
-					</span>
-				</div>
-				<div
-					className='carousel-inner'
-					style={{ transform: `translateX(-${activeIndex * 100}%)` }}>
-					{React.Children.map(children, (child, index) => {
-						return React.cloneElement(child, { width: "100%" })
-					})}
-				</div>
+  return (
+    <Fragment>
+      <div class="parent">
+        <div class="arrow">
+          <span
+            className="btn-carousel btn-carousel-left"
+            onClick={() => updateIndex(activeIndex - 1)}
+          >
+            <AiOutlineArrowLeft size="20px" />
+          </span>
+        </div>
+        <div className="carousel">
+          <div
+            className="carousel-inner"
+            style={{ transform: `translateX(-${activeIndex * 100}%)` }}
+          >
+            {React.Children.map(children, (child, index) => {
+              return React.cloneElement(child, { width: "100%" });
+            })}
+          </div>
+          <div className="indicators">
+            {React.Children.map(children, (child, index) => {
+              return (
+                <button
+                  className={`${index === activeIndex ? "active" : ""}`}
+                  onClick={() => updateIndex(index)}
+                ></button>
+              );
+            })}
+          </div>
+        </div>
+        <div class="arrow">
+          <span
+            className="btn-carousel btn-carousel-right"
+            onClick={() => updateIndex(activeIndex + 1)}
+          >
+            <AiOutlineArrowRight size="20px" />
+          </span>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
-				<div className='indicators'>
-					{React.Children.map(children, (child, index) => {
-						return (
-							<button
-								className={`${index === activeIndex ? "active" : ""}`}
-								onClick={() => updateIndex(index)}></button>
-						)
-					})}
-				</div>
-			</div>
-		</Fragment>
-	)
-}
-
-export default Carousel
+export default Carousel;
